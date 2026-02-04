84f72991104ffa5f2accacbbdd52add5099b9280
Zaid3480 <zaidmendajiwala3480@yahoo.com>
2026-02-04 21:02:03 +0530

    Add root endpoint for Koyeb routing

---
+++ src/main/java/com/itemmanagement/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.itemmanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Item Management API is running";
+    }
+}
